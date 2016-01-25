dacf50497e5c0056a624bb52593c67e8345b4102
lampropoi <lampropoi@gmail.com>
2016-01-25 11:57:44 +0200

    add docs/use remove()

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,8 @@
 /*	global $:false, document:false	*/
 
 $(document).ready(function() {
+// If the user has filled a new duty and presses Enter, then add it.
 	$('#duty').keypress(function(event) {
-//		debugger;
 		var code = event.keyCode || event.which;
 		if (code === 13 && this.value !== '') {
 			fillDutiesArea(this.value);
@@ -10,23 +10,29 @@ $(document).ready(function() {
 		}
 	});
 
+// If the user presses the complete button, move the duty in the doneArea.
 	$('#dutiesArea').on('click', '.complete', function() {
 		var doneDuty = $(this)
 										.closest('li')
 										.find('.dutyText');
 		$('<li></li>').append(doneDuty).appendTo('#doneArea');
-		$(this).closest('li').empty();
+		$(this).closest('li').remove();
 	});
 
+	// If the user presses the modify button, move the duty in the text area.
 	$('#dutiesArea').on('click', '.modify', function() {
 		$('#duty').val($(this).closest('li').find('.dutyText').html());
-		$(this).closest('li').empty();
+		$(this).closest('li').remove();
 	});
 
+	// If the user presses the delete button, delete the duty.
 	$('#dutiesArea').on('click', '.delete', function() {
-		$(this).closest('li').empty();
+		$(this).closest('li').remove();
 	});
-
+/**
+ * This function is used to create the li element in the dutiesArea.
+ * @param  {[Object]} duty [the added duty by the username]
+*/
 	function fillDutiesArea(duty) {
 		var spanCheck = $('<span></span>')
 										.append($('<img></img>')
